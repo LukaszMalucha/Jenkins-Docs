dc6c19e82430da64aa71d78db14afcf1c19e27fa
Lukasz Malucha <lucasmalucha@gmail.com>
2020-04-30 16:21:36 +0100

    data

--- app/2.py
+++ app/2.py
@@ -66,7 +66,7 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(port = 4000)
+    app.run(host='0.0.0.0', port=5000)
 
 
 
---
+++ app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, url_for, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+
+
+
+
+
+
+
+
+
+
+
